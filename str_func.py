e55ed636ec5c6a834e75b7854f8d7441bab39193
mariiasaliutina <m.salyutina@gmail.com>
2024-02-19 22:28:37 +0100

    attempt number 2 for a conflict

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def capslock(random_string):
+    '''
+    Hallo Welt
+    '''
     return(random_string.upper())
+
+
+def capitalize_first_letters(input_string):
+    '''
+    merhaba dünya
+    '''
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    result_string = ' '.join(capitalized_words)
+    return result_string
